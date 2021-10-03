21e0f0aff133173aa8701eabc579c2b24610e8de
Ankit_Susne <50927105+Anksus@users.noreply.github.com>
2021-10-04 01:14:50 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -21,7 +21,6 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">Home</Link>
         </div>
-        
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">Theme by timlrx</Link>
         </div>
